3a5ddc7ff96ddba3d83ed1ffa99ad5a079087915
Mia <mia@MacBook-Pro-de-Mia.local>
2019-05-04 23:49:06 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        titulo: 'GYN con Vue',
+        tareas:[],
+        nuevaTarea: ''
+    },
+    methods: {
+        agragarTarea: function(){
+            // console.log('click', this.nuevaTarea);
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+
+            })
+            // console.log(this.tareas)
+            this.nuevaTarea ='';
+        },
+
+        editarTarea: function(index){
+            // console.log('editar', index);
+            this.tareas[index].estado='true';
+        },
+        eliminar: function(index){
+            this.tareas.splice(index, 1);
+
+        },
+    },
+    created: function(){
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        console.log(datosDB);
+    }
+});
