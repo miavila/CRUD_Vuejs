afd39067f2d8e4b444e2c67f83c6b69ee2947a00
Mia <mia@MacBook-Pro-de-Mia.local>
2019-05-08 17:41:02 +0200

    Agregar localStorage

--- app.js
+++ app.js
@@ -7,27 +7,31 @@ const app = new Vue({
     },
     methods: {
         agragarTarea: function(){
-            // console.log('click', this.nuevaTarea);
             this.tareas.push({
                 nombre: this.nuevaTarea,
                 estado: false
 
             })
-            // console.log(this.tareas)
             this.nuevaTarea ='';
+            localStorage.setItem('crud-vue', JSON.stringify (this.tareas));
+
         },
 
         editarTarea: function(index){
-            // console.log('editar', index);
             this.tareas[index].estado='true';
+            localStorage.setItem('crud-vue', JSON.stringify (this.tareas));
         },
         eliminar: function(index){
             this.tareas.splice(index, 1);
-
+            localStorage.setItem('crud-vue', JSON.stringify (this.tareas));
         },
     },
     created: function(){
-        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
-        console.log(datosDB);
+        let datosDB = JSON.parse(localStorage.getItem('crud-vue'));
+        if (datosDB === null) {
+            this.tareas = [];
+        } else {
+            this.tareas = datosDB;
+        }
     }
 });
